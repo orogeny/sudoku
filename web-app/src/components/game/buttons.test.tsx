aa712474f9829ab9cfbc63208df6dbc8bb404985
Paul <paulwharin@gmail.com>
2024-05-13 06:04:49 +0100

    refactor: simplify ToggleableButton

--- web-app/src/components/game/buttons.test.tsx
+++ web-app/src/components/game/buttons.test.tsx
@@ -1,22 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, test, vi } from "vitest";
-import { Button, DigitButton, ToggleableButton } from "./buttons";
-
-describe("Buttons", () => {
-  test("should handle click twice", () => {
-    const clickHandler = vi.fn();
-
-    render(<Button onClick={clickHandler}>Hello!</Button>);
-
-    const helloButton = screen.getByText(/hello!/i);
-
-    expect(helloButton).toBeInTheDocument();
-
-    fireEvent.click(helloButton);
-    fireEvent.click(helloButton);
-    expect(clickHandler).toHaveBeenCalledTimes(2);
-  });
-});
+import { ToggleableButton } from "./buttons";
 
 describe("ToggleableButton", () => {
   test("should handle click", () => {
@@ -29,22 +13,6 @@ describe("ToggleableButton", () => {
     expect(toggle).toBeInTheDocument();
 
     fireEvent.click(toggle);
-
-    expect(clickHandler).toHaveBeenCalledOnce();
-  });
-});
-
-describe("DigitButton", () => {
-  test("should handle click", () => {
-    const clickHandler = vi.fn();
-
-    render(<DigitButton onClick={clickHandler}>9</DigitButton>);
-
-    const button = screen.getByText(/9/);
-
-    expect(button).toBeInTheDocument();
-
-    fireEvent.click(button);
 
     expect(clickHandler).toHaveBeenCalledOnce();
   });
--- web-app/src/components/game/buttons.tsx
+++ web-app/src/components/game/buttons.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, MouseEvent, useState } from "react";
+import { ComponentProps } from "react";
 import { cn } from "../../shared/cn";
 
 type ToggleableButtonProps = ComponentProps<"button"> & {
@@ -6,73 +6,16 @@ type ToggleableButtonProps = ComponentProps<"button"> & {
   toggledClassName?: string;
 };
 
-function Button({
-  children,
-  className,
-  ...restProps
-}: ComponentProps<"button">) {
-  return (
-    <button
-      className={cn(
-        "flex h-12 basis-1/4 items-center justify-around rounded bg-zinc-300 text-2xl font-semibold text-slate-800 shadow-sm hover:bg-zinc-400 active:bg-zinc-500",
-        "xl:h-16 xl:w-44",
-        className,
-      )}
-      {...restProps}
-    >
-      {children}
-    </button>
-  );
-}
-
-function DigitButton({
-  children,
-  className,
-  ...restProps
-}: ComponentProps<"button">) {
-  return (
-    <button
-      className={cn(
-        "flex aspect-square basis-1/9 items-center justify-center rounded-md bg-sky-200 text-2xl font-semibold text-slate-800 shadow-md hover:bg-sky-300 active:bg-sky-400 xl:h-16",
-        className,
-      )}
-      {...restProps}
-    >
-      {children}
-    </button>
-  );
-}
-
 function ToggleableButton({
   children,
   className,
-  onClick,
   toggledClassName,
   toggled = false,
   ...restProps
 }: ToggleableButtonProps) {
-  const [isToggled, setIsToggled] = useState(toggled);
-
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-
-    setIsToggled((_) => !isToggled);
-
-    if (onClick) {
-      onClick(e);
-    }
-  };
-
   return (
     <button
-      onClick={handleClick}
-      className={cn(
-        "flex h-12 basis-1/4 items-center justify-around rounded bg-zinc-300 text-2xl font-semibold text-slate-800 shadow-sm",
-        "xl:h-16 xl:w-44",
-        className,
-        isToggled && "bg-zinc-500 text-slate-100",
-        isToggled && toggledClassName,
-      )}
+      className={cn(className, toggled && toggledClassName)}
       {...restProps}
     >
       {children}
@@ -80,4 +23,4 @@ function ToggleableButton({
   );
 }
 
-export { Button, DigitButton, ToggleableButton, type ToggleableButtonProps };
+export { ToggleableButton, type ToggleableButtonProps };
